136b1d2d8418bf3c869d75c2d3607979d7938f81
ross0778 <ross0778@umn.edu>
2026-02-12 14:34:18 -0600

    Columns named correctly during data import

--- R/week4.R
+++ R/week4.R
@@ -3,4 +3,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
 # Data Import
-import_tbl <- read_delim("../data/week4.dat", delim="-", col_names=c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "qs"))
+import_tbl <- read_delim("../data/week4.dat", delim="-", col_names=c("casenum", "parnum", "stimver", "datadate", "qs"))
+glimpse(import_tbl)
