3bbadd21e8f2226051c12093c1cd9ca90f880477
ross0778 <ross0778@umn.edu>
2026-02-12 14:43:23 -0600

    Saved wide_tbl

--- R/week4.R
+++ R/week4.R
@@ -5,3 +5,4 @@ library(tidyverse)
 # Data Import
 import_tbl <- read_delim("../data/week4.dat", delim="-", col_names=c("casenum", "parnum", "stimver", "datadate", "qs"))
 glimpse(import_tbl)
+wide_tbl<- import_tbl %>% separate(qs, into=c("q1", "q2", "q3", "q4", "q5"), sep="-")
